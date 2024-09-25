8266fbae8cbaf52b386fa0daeaa2ccef508486e4
unknown <raupersan>
2024-09-25 20:59:38 +0200

    ejercicos terminados

--- p01/src/ej6/main6.java
+++ p01/src/ej6/main6.java
@@ -1,9 +1,36 @@
 package ej6;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class main6 {
+	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		String fichero1 = "";
+
+		System.out.println("Introduce el nombre del fichero que quieres crear");
+		fichero1 = sc.nextLine();
+		File fichero2 = new File(fichero1);
+		comprobarFichero(fichero2);
 
 	}
 
+	private static void comprobarFichero(File fichero) {
+		try {
+			if (!fichero.exists()) {
+				fichero.createNewFile();
+				System.out.println("Fichero creado");
+				fichero.setWritable(true);
+				fichero.setReadable(false);
+				fichero.setExecutable(false);
+				System.out.println("Cambiados los permisos a sólo lectura");
+
+			} else {
+				System.out.println("El fichero ya existe y esta es su ruta: " +fichero.getAbsolutePath());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
