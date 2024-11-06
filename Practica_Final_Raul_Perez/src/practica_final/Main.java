507d14ebc12258d61044d408eb704b6327471707
unknown <raupersan>
2024-11-06 19:39:22 +0100

    Nodos y lectura de etiquetas comentado

--- Practica_Final_Raul_Perez/src/practica_final/Main.java
+++ Practica_Final_Raul_Perez/src/practica_final/Main.java
@@ -16,16 +16,21 @@ public class Main {
 	}
 
 	private static void cargarEmpleados(String ruta) throws ParserConfigurationException, IOException ,  SAXException {
+        // Lista para almacenar las líneas que escribiremos en el archivo
         ArrayList<String> empleados= new ArrayList<>();
+        // Parsear el archivo XML
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(Paths.get(ruta).toFile());
+        // Normalizar el XML
         doc.getDocumentElement().normalize();
         NodeList listaClientes = doc.getElementsByTagName("Clientes");
+        // Recorrer cada nodo <empleado>
         for(int i=0; i< listaClientes.getLength(); i++) {
         	Node nodo = listaClientes.item(i);
             if (nodo.getNodeType() == Node.ELEMENT_NODE) {
             	Element cliente = (Element) nodo;
+                // Extraer los datos de cada <empleado>
             	String numCliente= cliente.getElementsByTagName("numerodecliente").item(i).getTextContent();
             }
         }
